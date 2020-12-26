7fedff0981a1c7559411e4396486de5e4b63092b
DAnna-17 <dracoann1712@gmail.com>
2020-12-26 14:29:46 +0300

    1

--- go_camera.py
+++ go_camera.py
@@ -8,7 +8,7 @@ class Board:
         self.map = "data\\" + map + '.txt'
 
     def render(self, screen):
-        t = {'#' : 'box.png', '*': 'grass.png'}
+        t = {'#': 'box.png', '*': 'grass.png'}
         all_sprites = pygame.sprite.Group()
         with open(self.map) as f:
             map = f.read().split('\n')
@@ -18,35 +18,65 @@ class Board:
                     sprite = pygame.sprite.Sprite()
                     sprite.image = load_image(image)
                     sprite.rect = sprite.image.get_rect()
-                    x, y = i * 50 + 50, k * 50 + 50
-                    print(x, y)
+                    x, y = i * 50, k * 50
                     sprite.rect.x, sprite.rect.y = x, y
                     all_sprites.add(sprite)
-            print(all_sprites)
         self.all_sprites = all_sprites
         all_sprites.draw(screen)
+
+    def apply(self, x, y, screen):
+        for sprite in self.all_sprites:
+            print(sprite.rect.x, sprite.rect.y)
+            sprite.rect.x -= x
+            sprite.rect.y -= y
+            print(sprite.rect.x, sprite.rect.y, '\n-----')
+            if sprite.rect.x < 12 or sprite.rect.x > 312 or sprite.rect.y < 7 or sprite.rect.y > 307:
+                sprite.rect.x= 1000
+                sprite.rect.y = 1000
+        self.all_sprites.draw(screen)
+
 
     def draw(self, screen):
         self.all_sprites.draw(screen)
 
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += 100#self.dx
+        obj.rect.y += 100#self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - 50 // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - 50 // 2)
 
 class Hero:
     def __init__(self, x, y):
         self.all_sprites = pygame.sprite.Group()
         sprite = pygame.sprite.Sprite()
         self.sprite = sprite
-        sprite.image = load_image('mar.png')
+        self.image = load_image('mar.png')
+        sprite.image = self.image
         sprite.rect = sprite.image.get_rect()
         self.x, self.y = x, y
         sprite.rect.x, sprite.rect.y = self.x, self.y
+        self.rect = self.image.get_rect()
         self.all_sprites.add(sprite)
         self.all_sprites.draw(screen)
 
-    def go(self, x, y):
-        self.sprite.rect.x, self.sprite.rect.y = x, y
+    def render(self):
         self.all_sprites.draw(screen)
 
-    def render(self):
+    def go(self, x, y):
+        self.sprite.rect.x += x
+        self.sprite.rect.y += y
+        print(111111111111111111, self.sprite.rect.x, self.sprite.rect.y, x, y)
         self.all_sprites.draw(screen)
 
 
@@ -59,20 +89,22 @@ def load_image(name, colorkey=None):
     return image
 
 
-map = input('Введите название карты без указания типа файла\n')
+map = 'карта'#input('Введите название карты без указания типа файла\n')
 pygame.init()
 board = Board(map)
-screen = pygame.display.set_mode((700, 600))
+cam = Camera()
+screen = pygame.display.set_mode((350, 350))
 board.render(screen)
 pygame.display.flip()
-x, y = 62, 57
-h = Hero(x, y)
+h = Hero(162, 157)
+cam.update(h.sprite)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
+            x, y = 0, 0
             if event.key == pygame.K_LEFT:
                 x -= 50
             elif event.key == pygame.K_RIGHT:
@@ -81,16 +113,14 @@ while running:
                 y += 50
             elif event.key == pygame.K_UP:
                 y -= 50
-            if x < 62:
-                x= 62
-            elif x > 512:
-                x = 512
-            if y < 57:
-                y = 57
-            elif y > 507:
-                y = 507
-            h.go(x, y)
-    board.draw(screen)
+            #h.go(x, y)
+            cam.update(h)
+            screen.fill((0, 0, 0))
+            board.apply(x, y, screen)
+            # board.render(screen)
+            pygame.display.flip()
+            print(11111111111111)
+    # board.render(screen)
     h.render()
     pygame.display.flip()
-    screen.fill((0, 0, 0))
+    #screen.fill((0, 0, 0))
